f3f44d94c8e4716e71931255546204c1a34c4524
Dougdevs <douglas.r18@aluno.ifsc.edu.br>
2023-08-10 08:44:11 -0300

    viagens

---
+++ README.md
@@ -0,0 +1,3 @@
+# A simple landing page developed in react!
+
+Responsive!!!
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Desenvolvido por SC DataLink</p>
+    </div>
+  );
+};
+
+export default Footer;
